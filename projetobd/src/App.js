f57bdba09f6b45ba13a943aa764f9dcd55895160
caio theodoro <caio-eduardotheodoro@hotmail.com>
2019-11-23 21:53:49 -0300

    crud

---
+++ projetobd/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import UserBar from './components/navbar/index'
+function App() {
+  return (
+    <div className="App">
+      <UserBar></UserBar>
+    </div>
+  );
+}
+
+export default App;
